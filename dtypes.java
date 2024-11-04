bd4113abf582af2e20c3faad8219e0d4603c15cd
Srinath <devarapallisrinath8@gmail.com>
2024-11-04 11:33:47 +0530

    Day1 Morning session

---
+++ dtypes.java
@@ -0,0 +1,24 @@
+public class dtypes {
+    public static void main(String[] args) {
+      System.out.println("The size of the byte: " + Byte.SIZE / 8 + " bytes = " + Byte.SIZE + " bits");
+      System.out.println("Range of Byte: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the short: " + Short.SIZE / 8 + " bytes = " + Short.SIZE + " bits");
+      System.out.println("Range of Short: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the integer: " + Integer.SIZE / 8 + " bytes = " + Integer.SIZE + " bits");
+      System.out.println("Range of Integer: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the long: " + Long.SIZE / 8 + " bytes = " + Long.SIZE + " bits");
+      System.out.println("Range of Long: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the float: " + Float.SIZE / 8 + " bytes = " + Float.SIZE + " bits");
+      System.out.println("Range of Float: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the double: " + Double.SIZE / 8 + " bytes = " + Double.SIZE + " bits");
+      System.out.println("Range of Double: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+      System.out.println();
+      System.out.println("The size of the char: " + Character.SIZE / 8 + " bytes = " + Character.SIZE + " bits");
+      System.out.println("Range of Character: " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+    }
+  }
---
+++ swap2.java
@@ -0,0 +1,16 @@
+// package codevita.TCS-CodeVita-Training;
+import java.util.Scanner;
+public class swap2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter A:");
+        int a=sc.nextInt();
+        System.out.println("Enter B:");
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+    }
+}
